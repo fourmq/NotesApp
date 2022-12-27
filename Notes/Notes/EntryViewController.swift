697508b63d89ac1749fe2a644bf94e4d972bf626
fourm <67043773+fourmq@users.noreply.github.com>
2022-12-27 17:45:16 +0300

    Add files via upload

---
+++ Notes/Notes/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Notes
+//
+//  Created by Alexey Filippov on 27.12.2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+    
+    public var completion: ((String, String) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Сохранить", style: .done, target: self, action: #selector(didTapSave))
+    }
+    
+    @objc func didTapSave() {
+        if let text = titleField.text, !text.isEmpty, !noteField.text!.isEmpty {
+            completion?(text, noteField.text!)
+        }
+    }
+
+}
---
+++ Notes/Notes/NoteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by Alexey Filippov on 27.12.2022.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = "" 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+        
+    }
+    
+
+}
